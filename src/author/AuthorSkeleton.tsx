9e61dd5a19bbf15c5a5b9fff8c31c6d3f0fa37b0
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-08-19 12:21:19 +0200

    create author skeleton

---
+++ src/author/AuthorSkeleton.tsx
@@ -0,0 +1,8 @@
+export default function AuthorSkeleton() {
+  return (
+    <section className='author'>
+      <div className='author__name author__name--skeleton'></div>
+      <div className='author__username author__username--skeleton'></div>
+    </section>
+  );
+}
